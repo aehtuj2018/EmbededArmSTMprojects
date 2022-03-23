f1f4461f7bf9a69193afc7f0f7435bb0e8b2eff3
Tujuba (Admin) <A2012521@nventco.com>
2022-03-23 13:10:36 -0600

    Added and Configured FreeRTOS, on STMCubeIDE

---
+++ FreeRTOS/freeRTOS_Project/Debug/ThirdParty/FreeRTOS/queue.d
@@ -0,0 +1,23 @@
+ThirdParty/FreeRTOS/queue.o: ../ThirdParty/FreeRTOS/queue.c \
+ C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/include/projdefs.h \
+ C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/include/portable.h \
+ C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/include/task.h \
+ C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/include/list.h \
+ C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/include/queue.h \
+ C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/include/task.h
+C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/include/projdefs.h:
+C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/include/portable.h:
+C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/include/task.h:
+C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/include/list.h:
+C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/include/queue.h:
+C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/include/task.h:
---
+++ FreeRTOS/freeRTOS_Project/Debug/ThirdParty/FreeRTOS/tasks.d
@@ -0,0 +1,25 @@
+ThirdParty/FreeRTOS/tasks.o: ../ThirdParty/FreeRTOS/tasks.c \
+ C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/include/projdefs.h \
+ C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/include/portable.h \
+ C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/include/task.h \
+ C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/include/list.h \
+ C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/include/timers.h \
+ C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/include/task.h \
+ C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/include/stack_macros.h
+C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/include/projdefs.h:
+C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/include/portable.h:
+C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/include/task.h:
+C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/include/list.h:
+C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/include/timers.h:
+C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/include/task.h:
+C:/CodeLib/ARM-Cortex4-STMF4/EmbededArmSTMprojects/FreeRTOS/freeRTOS_Project/ThirdParty/FreeRTOS/include/stack_macros.h:
